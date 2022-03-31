50b13eed506919bc9f4c34a89097ab25fcff0b0e
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-03-31 14:59:07 +0000

    123

--- 2.py
+++ 2.py
@@ -2,6 +2,14 @@ for x in 0,1:
   for y in 0,1:
     for z in 0,1:
       for w in 0,1:
-        F = ((x <= y) == (z or w)) or (x <= z)
+        F = ((x <= y) == (z and w)) and (x <= z)
         if F == 1:
           print(y,z,x,w)
+
+# 0 X 0 X
+# X X X 0
+# 0 X X 0
+
+# 0 0 0 0
+# 1 0 0 0
+# 0 1 1 0
